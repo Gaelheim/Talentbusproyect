83a356b7d566f5f3ced62f80fb53f80a10f10417
Angel Daniel <Jari_Boo09@hotmail.com>
2026-04-07 11:56:11 -0400

    actu CN

---
+++ CapaNegocios/CN_Asignaciones.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using CapaDatos;
+
+namespace CapaNegocios
+{
+    public class CN_Asignaciones
+    {
+        private CD_Asignaciones objetoCD = new CD_Asignaciones();
+
+        public DataTable MostrarAsignaciones()
+        {
+            DataTable tabla = new DataTable();
+            tabla = objetoCD.MostrarAsignaciones();
+            return tabla;
+        }
+
+        public void InsertarAsignacion(string Nombre, decimal Porcentaje, string Descripcion)
+        {
+            objetoCD.InsertarAsignacion(Nombre, Porcentaje, Descripcion);
+        }
+
+        public void EditarAsignacion(int Id, string Nombre, decimal Porcentaje, string Descripcion)
+        {
+            objetoCD.EditarAsignacion(Id, Nombre, Porcentaje, Descripcion);
+        }
+
+        public void EliminarAsignacion(string Id)
+        {
+            objetoCD.EliminarAsignacion(Id);
+        }
+    }
+}
---
+++ CapaNegocios/CN_Deducciones.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using CapaDatos;
+
+
+namespace CapaNegocios
+{
+    public class CN_Deducciones
+    {
+        private CD_Deducciones objetoCD = new CD_Deducciones();
+        public System.Data.DataTable MostrarDeducciones()
+        {
+            System.Data.DataTable tabla = new System.Data.DataTable();
+            tabla = objetoCD.MostrarDeducciones();
+            return tabla;
+        }
+        public void InsertarDeduccion(string Nombre, decimal Porcentaje, string Descripcion)
+        {
+            objetoCD.InsertarDeduccion(Nombre, Porcentaje, Descripcion);
+        }
+        public void EditarDeduccion(int Id, string Nombre, decimal Porcentaje, string Descripcion)
+        {
+            objetoCD.EditarDeduccion(Id, Nombre, Porcentaje, Descripcion);
+        }
+        public void EliminarDeduccion(string Id)
+        {
+            objetoCD.EliminarDeduccion(Id);
+        }
+    }
+}
